f6e65fce1bbf479cba6ed28145004ad886b4954c
server_42 <devbhattacharya42@gmail.com>
2023-09-19 20:01:26 +0530

    App done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
-const JWT_SECRET = "dev's-secret"
+require('dotenv').config()
 
 // custom modules created by me :)
 const {connection} = require('./db/conn')
@@ -73,13 +73,35 @@ app.post('/api/login', async (req, res) => {
         const token = jwt.sign({
             id: user._id,
             username: user.username
-        }, JWT_SECRET);
+        }, process.env.JWT_SECRET);
         return res.send({ status: 'ok', data: token }); // Return the response here
     } else {
         // Display unsuccessful message
         return res.send({ status: 'error', error: 'Invalid token' }); // Return the response here
     }
 });
+
+// Changing user's password if login is successfully done and the user receives a token
+
+app.get('/api/change-password', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'change-password.html'));
+  });
+
+app.post('/api/change-password' , async(req,res) => {
+    const {token,newpassword} = req.body
+    try{
+        const user = jwt.verify(token,process.env.JWT_SECRET)
+
+        const _id = user.id
+        const hashedPassword = await bcrypt.hashSync(newpassword,10)
+        await UserModel.updateOne({_id} , {$set : {password : hashedPassword}})
+        res.send({status : 'ok' , message : 'password successfully updated'})
+    }catch(error){
+        console.log(error)
+        res.send({status : 'error' , error : 'phishing'})
+    }
+})
+
 
 app.listen(8000, async() => {
     try{
